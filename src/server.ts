926a6f1d07caaf405b80eb409a27eeb2ffa60eb5
Jaqueline Vargas <jaquelinevargas015@gmail.com>
2026-02-20 07:19:51 -0300

    adicionada ordenação, gênero e lingua do filme

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,17 @@ import { PrismaClient } from "./generated/prisma";
  const app = express();
  const prisma = new PrismaClient();
 
- app.get("/movies", async (req, res) => {
-   const movies = await prisma.movie.findMany();
+ app.get("/movies", async (_, res) => {
+   const movies = await prisma.movie.findMany({
+    orderBy: {
+      title: "asc" 
+    },
+    include: {
+      genres:true,
+      languages: true
+    }
+   }
+   );
 
    res.json(movies);
 
