0f6b191735dbacdc2c4ac8f952e8df1968bfe06d
Jaqueline Vargas <jaquelinevargas015@gmail.com>
2026-02-22 09:41:53 -0300

    Adicionada atualização de filmes

--- src/server.ts
+++ src/server.ts
@@ -8,51 +8,81 @@ const prisma = new PrismaClient()
 app.use(express.json())
 
 app.get("/movies", async (_, res) => {
-    const movies = await prisma.movie.findMany({
-        orderBy: {
-            title: "asc",
-        },
-        include: {
-            genres: true,
-            languages: true,
-        },
-    })
+   const movies = await prisma.movie.findMany({
+      orderBy: {
+         title: "asc",
+      },
+      include: {
+         genres: true,
+         languages: true,
+      },
+   })
 
-    res.json(movies)
+   res.json(movies)
 })
 
 app.post("/movies", async (req, res) => {
-    const { title, genre_id, language_id, oscar_count, release_date } = req.body
+   const { title, genre_id, language_id, oscar_count, release_date } = req.body
 
-    try {
-        const movieWithSameTitle = await prisma.movie.findFirst({
-            where: { title: { equals: title, mode: "insensitive" } }, // Faz com que não tenha diferença entre maiúsculas e minúsculas, ou seja, se o filme for escrito com maiscula ou minuscula, ele não vai deixar cadastradar se for o mesmo titulo.
-        })
+   try {
+      const movieWithSameTitle = await prisma.movie.findFirst({
+         where: { title: { equals: title, mode: "insensitive" } }, // Faz com que não tenha diferença entre maiúsculas e minúsculas, ou seja, se o filme for escrito com maiscula ou minuscula, ele não vai deixar cadastradar se for o mesmo titulo.
+      })
 
-        if (movieWithSameTitle) {
-            return res
-                .status(409)
-                .send({
-                    message: "Já existe um filme cadastrado com esse título",
-                })
-        }
+      if (movieWithSameTitle) {
+         return res.status(409).send({
+            message: "Já existe um filme cadastrado com esse título",
+         })
+      }
 
-        await prisma.movie.create({
-            data: {
-                title: title,
-                genre_id: genre_id,
-                language_id: language_id,
-                oscar_count: oscar_count,
-                release_date: new Date(release_date),
-            },
-        })
-    } catch (error) {
-        return res.status(500).send({ message: "Falha ao cadastrar um filme" })
-    }
+      await prisma.movie.create({
+         data: {
+            title: title,
+            genre_id: genre_id,
+            language_id: language_id,
+            oscar_count: oscar_count,
+            release_date: new Date(release_date),
+         },
+      })
+   } catch (error) {
+      return res.status(500).send({ message: "Falha ao cadastrar um filme" })
+   }
 
-    res.status(201).send()
+   res.status(201).send()
+})
+
+app.put("/movies/:id", async (req, res) => {
+   const id = Number(req.params.id)
+
+   try {
+      const movie = await prisma.movie.findUnique({
+         where: {
+            id: id,
+         },
+      })
+
+      if (!movie) {
+         return res.status(404).send({ message: "Filme não encontrado" })
+      }
+
+      const data = { ...req.body }
+      data.release_date = data.release_date
+         ? new Date(data.release_date)
+         : undefined
+
+      await prisma.movie.update({
+         where: {
+            id: id,
+         },
+         data: data,
+      })
+   } catch (error) {
+      return res.status(500).send({ message: "Falha ao atualizar um filme" })
+   }
+
+   res.status(200).send()
 })
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
+   console.log(`Server is running on port ${port}`)
 })
