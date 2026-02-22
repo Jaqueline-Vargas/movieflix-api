e25706074dfefb019598167ee5a84740dfb8bd5b
Jaqueline Vargas <jaquelinevargas015@gmail.com>
2026-02-22 11:04:51 -0300

    adicionada a remoção de filmes

--- src/server.ts
+++ src/server.ts
@@ -83,6 +83,24 @@ app.put("/movies/:id", async (req, res) => {
    res.status(200).send()
 })
 
+app.delete("/movies/:id", async (req, res) => {
+   const id = Number(req.params.id)
+
+   try {
+      const movie = await prisma.movie.findUnique({ where: { id } })
+
+      if (!movie) {
+         return res.status(404).send({ message: "Filme não encontrado" })
+      }
+
+      await prisma.movie.delete({ where: { id } })
+   } catch (error) {
+      return res.status(500).send({ message: "Falha ao deletar um filme" })
+   }
+
+   res.status(200).send()
+})
+
 app.listen(port, () => {
    console.log(`Server is running on port ${port}`)
 })
